243c730df53fb74dd7868b1a2dd0ffab1e19be51
OG Novuh <jamooney2000@gmail.com>
2018-02-02 20:22:25 -0500

    Update to language getting.

--- src/examples/langGet.js
+++ src/examples/langGet.js
@@ -1,19 +1,15 @@
 /**
- * Grab the server settings.
+ * Require the server language.
  */
-const settings = this.client.settings.get(message.guild.id);
 
-/**
- * Declare the servers language based on the lang key in the settings.
- */
-const serverLang = `${settings.lang}`;
+const { lang } = this.client.settings.get(message.guild.id);
 
 /**
  * Require the language file.
  */
-const lang = require(`../../languages/${serverLang}/${this.help.category}/${this.help.category}.json`);
+const cmdLang = require(`../../languages/${lang}/${this.help.category}/${this.help.category}.json`);
 
 /**
  * Require the general error file for basic errors.
  */
-const generalErr = require(`../../languages/${serverLang}/general.json`);
+const generalErr = require(`../../languages/${lang}/general.json`);
