0eb04aaac84f09eba415ead4f348849f0b136d66
OG Novuh <jamooney2000@gmail.com>
2018-02-05 15:59:24 -0500

    Update deps, eval, create Stopwatch.

--- src/commands/System/eval.js
+++ src/commands/System/eval.js
@@ -1,4 +1,5 @@
-const Command = require('../base/Command.js');
+const Command = require('../../base/Command.js');
+const Stopwatch = require('../../util/Stopwatch.js');
 
 class Eval extends Command {
   constructor(client) {
@@ -6,21 +7,30 @@ class Eval extends Command {
       name: 'eval',
       description: 'Evaluates arbitrary Javascript.',
       category:'System',
-      usage: 'eval <expression>',
+      usage: 'eval <expression:string>',
       aliases: ['ev'],
       permLevel: 'Bot Owner'
     });
   }
 
-  async run(message, args, level) { // eslint-disable-line no-unused-vars
-    const code = args.join(' ');
+  async run(message, [code], level) { // eslint-disable-line no-unused-vars     
+    const stopwatch = new Stopwatch();
+    let syncTime, asyncTime;    
     try {
       const evaled = eval(code);
       const clean = await this.client.clean(this.client, evaled);
-      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
+      syncTime = stopwatch.friendlyDuration;
+      const msg = await message.channel.send('Evaling.');
+      stopwatch.stop();
+      const time = this.formatTime(syncTime, asyncTime);
+      msg.edit(`\`\`\`js\n${clean}\n\`\`\`\n\n${time}`);
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${await this.client.clean(this.client, err)}\n\`\`\``);
     }
+  }
+
+  formatTime(syncTime, asyncTime) {
+    return asyncTime ? `⏱ ${asyncTime}<${syncTime}>` : `⏱ ${syncTime}`;
   }
 }
 
